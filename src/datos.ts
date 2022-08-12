89b2b41d96a3f6b11d6c41f4f6fad5ec46f80c3a
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-08-12 10:49:14 -0300

    Area del Rectngulo Ejercicio de Imputs y calculo d Area de un Rectangulo

--- src/datos.ts
+++ src/datos.ts
@@ -1,12 +1,21 @@
 import "./styles.css";
 
-let btnEnv = document.getElementById("btnEnviar");
+let btnBase = document.getElementById("btnBase");
 let dato = document.getElementById("dato");
 let rotulo = document.getElementById("rotulo");
-//rotulo.innerHTML = "Ingrese un dato: ";
+rotulo.innerHTML = "Ingrese un dato: ";
 
-btnEnv.addEventListener("click" , () => {
-  console.log("el dato ingresado es ",dato.value);
+
+btnBase.addEventListener("click" , () => {
+  console.log("La Base es ",dato.value);
 }
 
+let btnAltura = document.getElementById("btnAltura");
+let dato2 = document.getElementById("dato2");
+let rotulo2 = document.getElementById("rotulo2");
+rotulo2.innerHTML = "Ingrese un dato2: ";
+
+btnAltura.addEventListener("click" , () => {
+  console.log("La altura es ",dato2.value);
+}
 
